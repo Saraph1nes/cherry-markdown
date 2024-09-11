1f9fc6725680954e907b90cd9a8bd2036f3803b0
Saraph1nes <xiayx.leo@qq.com>
2024-09-11 16:13:01 +0800

    feat: 支持设置编辑器值

--- src/Editor.js
+++ src/Editor.js
@@ -725,4 +725,11 @@ export default class Editor {
     this.options.writingStyle = writingStyle;
     this.initWritingStyle();
   }
+
+  /**
+   * 支持设置编辑器值
+   */
+  setValue(value = '') {
+    this.editor.setOption('value', value);
+  }
 }
