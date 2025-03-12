1e08198e3fd568691894541472ca4acf5207d3dc
Saraph1nes <xiayx.leo@gmail.com>
2025-03-12 15:55:04 +0800

    feat: 添加关于页面提示框

--- packages/client/src/utils/listener.ts
+++ packages/client/src/utils/listener.ts
@@ -1,8 +1,6 @@
 import { listen } from '@tauri-apps/api/event';
-import { WebviewWindow } from '@tauri-apps/api/webviewWindow';
 import Cherry from 'cherry-markdown';
-import { invoke } from '@tauri-apps/api/core';
-import { newFile, openFile, saveAsNewMarkdown, saveMarkdown } from './handler';
+import { handleAboutMenu, handleToggleToolbar, newFile, openFile, saveAsNewMarkdown, saveMarkdown } from './handler';
 import type { FileStore } from '../store';
 
 const initListener = (cherryMarkdown: Cherry, fileStore: FileStore) => {
@@ -12,34 +10,10 @@ const initListener = (cherryMarkdown: Cherry, fileStore: FileStore) => {
     // todo: 1如果没有文件路径，就弹出转移到另存为；2只有在被更改过的情况下才进行保存；3这里要改变save的按钮disabled状态
     saveMarkdown(cherryMarkdown, fileStore);
   });
-  listen('save_as', async () => saveAsNewMarkdown(cherryMarkdown, fileStore));
-  listen('toggle_toolbar', async () => {
-    const cherryNoToolbar = document.querySelector('.cherry--no-toolbar');
-    console.log(cherryNoToolbar, !cherryNoToolbar);
-    await invoke('get_show_toolbar', { show: !!cherryNoToolbar });
-    cherryMarkdown.toolbar.toolbarHandlers.settings('toggleToolbar');
-  });
+  listen('save_as', () => saveAsNewMarkdown(cherryMarkdown, fileStore));
+  listen('toggle_toolbar', () => handleToggleToolbar(cherryMarkdown));
   // 关于窗口
-  listen('about_menu', async () => {
-    const window = new WebviewWindow('about', {
-      url: '/about',
-      width: 400,
-      height: 300,
-      title: '关于',
-      resizable: false,
-      skipTaskbar: true,
-      decorations: true,
-      center: false,
-      alwaysOnTop: true,
-      focus: true,
-    });
-    window.once('tauri://created', async () => {
-      console.log('tauri://created');
-    });
-    window.once('tauri://error', async (error) => {
-      console.log('window create error!', error);
-    });
-  });
+  listen('about_menu', handleAboutMenu);
 };
 
 export default initListener;
